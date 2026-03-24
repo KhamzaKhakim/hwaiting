96a1dcbcb05af1c4d2b8b45d36c4e378928c9a37
Khamza Khakim <hamzir.04@gmail.com>
2026-03-24 20:14:00 +0500

    Improve columns

--- src/sheets/create.ts
+++ src/sheets/create.ts
@@ -5,6 +5,8 @@ export async function createSpreadsheet(title: string): Promise<string> {
 
   console.log("token: ", token);
 
+  //   Job Title, Company, Needed Experience, Tech Stack
+
   const response = await fetch(
     "https://sheets.googleapis.com/v4/spreadsheets",
     {
@@ -14,13 +16,44 @@ export async function createSpreadsheet(title: string): Promise<string> {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        properties: { title },
+        properties: { title }, // workbook name
+        sheets: [
+          {
+            properties: {
+              title: "hwaiting",
+            },
+            data: [
+              {
+                startRow: 0,
+                startColumn: 0,
+                rowData: [
+                  {
+                    values: [
+                      { userEnteredValue: { stringValue: "Job Title" } },
+                      { userEnteredValue: { stringValue: "Company" } },
+                      { userEnteredValue: { stringValue: "Application Date" } },
+                      {
+                        userEnteredValue: { stringValue: "Needed Experience" },
+                      },
+                      {
+                        userEnteredValue: { stringValue: "Tech Stack" },
+                      },
+                      {
+                        userEnteredValue: { stringValue: "Status" },
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
       }),
     },
   );
 
   const data = await response.json();
-  console.log(response.json());
+  console.log(data);
   console.log("Spreadsheet ID: " + data.spreadsheetId);
   return data.spreadsheetId;
 }
